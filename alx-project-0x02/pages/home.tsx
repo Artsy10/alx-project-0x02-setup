11b9636dbe3ba998bfeec653bc0d90f1d2a636e9
hiba <hibajira123@gmaim.com>
2025-06-09 01:05:17 +0100

    update

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,6 +1,6 @@
 // pages/home.tsx
 import React from 'react';
-import Header from '../components/layout/Header';
+import Header from '@/components/layout/Header';
 
 const Home = () => {
   return (
